1a4abd967065eda57727e516ae4872378a65c758
A-Muhammadali <110370469+A-Muhammadali@users.noreply.github.com>
2023-08-26 13:53:14 +0500

    Update basic_func13.py

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,4 @@
+form math import sqrt
 def main(a):
     '''find the square root of a number and return it.
     
@@ -7,5 +8,5 @@ def main(a):
     Returns:
         float: the absolute value.
     '''
-    return a/3
+    return math.sqrt(a,2)
 print(main(9))
